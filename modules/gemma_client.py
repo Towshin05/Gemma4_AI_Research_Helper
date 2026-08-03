f6038eb44c44ee3fdaf5f738b06d598c45bd46ab
Towshin05 <u2004086@student.cuet.ac.bd>
2026-08-03 11:16:23 +0600

    streamlit secrets

--- modules/gemma_client.py
+++ modules/gemma_client.py
@@ -7,7 +7,7 @@
 # class GemmaClient:
 #     def __init__(self):
 #         load_dotenv()
-        
+
 #         self.api_key=os.getenv("GOOGLE_API_KEY")
 #         self.client=genai.Client(api_key=self.api_key)
 #         self.model="gemini-2.5-flash"
@@ -16,13 +16,14 @@
 #             model=self.model,
 #             contents=prompt
 #         )
-        
+
 #         return response.text
 
 
 import os
 from dotenv import load_dotenv
 from openai import OpenAI
+import streamlit as st
 
 
 class GemmaClient:
@@ -30,7 +31,7 @@ class GemmaClient:
         load_dotenv()
 
         self.client = OpenAI(
-            api_key=os.getenv("OPENROUTER_API_KEY"),
+            api_key=os.secrets("OPENROUTER_API_KEY"),
             base_url="https://openrouter.ai/api/v1",
         )
 
@@ -40,16 +41,9 @@ class GemmaClient:
 
         response = self.client.chat.completions.create(
             model=self.model,
-            messages=[
-                {
-                    "role": "user",
-                    "content": prompt
-                }
-            ],
+            messages=[{"role": "user", "content": prompt}],
             temperature=0.2,
             max_tokens=4096,
         )
 
         return response.choices[0].message.content
-    
-    
